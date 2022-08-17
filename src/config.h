ed413935cf1b46d78fd4c5f4343c5a33962c1fd5
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-08-17 21:28:06 +0800

    Remove natively defined pins

--- src/config.h
+++ src/config.h
@@ -1,19 +1,18 @@
+#ifndef CONFIG_H
+#define CONFIG_H
+
+#include <Arduino.h>
+
 // ************************
 // Pins
 // ************************
 // SPI (VSPI) for SD and MCP41010 Digital POT
-#define PIN_SCK 18
-#define PIN_MOSI 23
-#define PIN_MISO 19
-#define PIN_SD_SS 5
+#define PIN_SD_SS SS // 5
 // digital potentiometer
-#define PIN_VR_A_SS 12
-#define PIN_VR_B_SS 14
-#define PIN_VR_C_SS 27
-#define PIN_VR_D_SS 26
-// I2C for MAX17043
-#define PIN_SDA 21
-#define PIN_SCL 22
+#define PIN_DPOT_A_SS 12
+#define PIN_DPOT_B_SS 14
+#define PIN_DPOT_C_SS 27
+#define PIN_DPOT_D_SS 26
 // PCM5102 I2S DAC
 #define PIN_DAC_DOUT 16
 #define PIN_DAC_LCK 17
@@ -24,8 +23,10 @@
 #define PIN_74HC595_SHCP 33
 
 // ************************
-// Internet Services Configure
+// Internet Services Configuration
 // ************************
 #define DNS_PORT 53
 #define WIFI_SSID "research"
 #define WIFI_PASSWORD "Skills39"
+
+#endif // CONFIG_H
