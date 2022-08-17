1404b2128b3c7f35e8a3c2fc15c84b474a331605
Chenghsuan Ho <chenghsuan@protonmail.com>
2022-08-17 21:27:17 +0800

    Add Dpot, a MCP41010 library

--- lib/Dpot/Dpot.cpp
+++ lib/Dpot/Dpot.cpp
@@ -1,1 +1,30 @@
 #include "Dpot.h"
+#include <Arduino.h>
+
+void Dpot::begin()
+{
+    pinMode(_ss, OUTPUT);
+    SPI.begin();
+}
+
+void Dpot::write(uint8_t value)
+{
+    Dpot::_writeReg(DPOT_CMD_WRITE, value);
+}
+
+void Dpot::open()
+{
+    Dpot::_writeReg(DPOT_CMD_SHUTDOWN, 1);
+}
+
+void Dpot::_writeReg(uint8_t command, uint8_t value)
+{
+    SPI.beginTransaction(_SETTINGS);
+    digitalWrite(_ss, LOW);
+
+    SPI.write16(command);
+    SPI.write(value);
+
+    digitalWrite(_ss, HIGH);
+    SPI.endTransaction();
+}
--- lib/Dpot/Dpot.h
+++ lib/Dpot/Dpot.h
@@ -1,6 +1,26 @@
+// Dpot.h: MCP41010 library
+
+#ifndef DPOT_H
+#define DPOT_H
+
 #include <Arduino.h>
+#include <SPI.h>
+
+#define DPOT_CMD_WRITE 0x11
+#define DPOT_CMD_SHUTDOWN 0x21
 
 class Dpot
 {
-    // TODO:
+public:
+    Dpot(int8_t ss = SS) : _ss(ss) {}
+    void begin();
+    void write(uint8_t value);
+    void open();
+
+private:
+    const SPISettings _SETTINGS;
+    int8_t _ss;
+    void _writeReg(uint8_t command, uint8_t value);
 };
+
+#endif // DPOT_H
